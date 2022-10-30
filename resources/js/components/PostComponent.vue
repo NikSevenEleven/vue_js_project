f45ff746c80127bc02142f88eb93961f72c11092
NikSevenEleven <kursdz@mail.ru>
2022-10-30 19:02:25 +0300

    data methods

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <SinglePostComponent></SinglePostComponent>
+    <div>Name:{{name}}</div>
+    <button @click="sayHello">SayHello</button>
+    <button @click="sayHi">SayHi</button>
+    <p id="demo"> </p>
+</div>
+</template>
+
+<script>
+import SinglePostComponent from "./SinglePostComponent";
+export default{
+    name:"PostComponent",
+
+    data() {
+    return {
+        name: 'Vasia',
+        age : 20,
+        arr:[
+            [
+
+            ]
+        ],
+
+         }
+    },
+
+    methods: {
+        sayHello() {
+            console.log('Hello')
+        },
+        sayHi () {
+         console.log('hi')
+        },
+    },
+
+    components:{
+            SinglePostComponent
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SinglePostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    single post
+</div>
+</template>
+
+<script>
+export default{
+    name:"SinglePostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
